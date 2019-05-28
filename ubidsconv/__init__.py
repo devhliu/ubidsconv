76d554156dd67d6ca62f200c091d9b99fd81009c
hui.liu02_CRC <hui.liu02@united-imaging.com>
2019-05-28 16:42:04 +0800

    init

---
+++ ubidsconv/__init__.py
@@ -0,0 +1,11 @@
+#----------------------------------------------------------------------------------------
+#
+#   Project - ubidsconv
+#   Description:
+#       A python processing package to convert UIH DICOM into bids format
+#   File Name:    __init__.py
+#   Purpose:
+#       to convert UIH specific dicom storage into bids format
+#   Author: hui.liu02@united-imaging.com
+#   Created 2019-04-19
+#----------------------------------------------------------------------------------------
