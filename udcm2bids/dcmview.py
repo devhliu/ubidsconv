9fa1fee44d54d07feeda7196a26352d1aef8126e
hui.liu02_CRC <hui.liu02@united-imaging.com>
2019-06-01 06:57:53 +0800

    u

--- udcm2bids/dcmview.py
+++ udcm2bids/dcmview.py
@@ -11,6 +11,7 @@
 #----------------------------------------------------------------------------------------
 
 import os
+import difflib
 import pydicom
 import shutil
 
@@ -62,16 +63,60 @@ def cp_series(dcm_root, series_tag_xlsxfile, tar_root):
     return
 #----------------------------------------------------------------------------------------
 #
+def diff_dcmfiles(dcm_file_0, dcm_file_1):
+    """
+    :param dcm_file_0:
+    :param dcm_file_1:
+    :return:
+    """
+    ds_0 = pydicom.read_file(dcm_file_0, force=True)
+    ds_1 = pydicom.read_file(dcm_file_1, force=True)
+    dss = tuple([ds_0, ds_1])
+    rep = []
+    for ds in dss:
+        lines = str(ds).split("\n")
+        lines = [line + "\n" for line in lines]  # add the newline to end
+        rep.append(lines)
+    diff = difflib.Differ()
+    for line in diff.compare(rep[0], rep[1]):
+        if line[0] != "?": print(line)
+    return
+#----------------------------------------------------------------------------------------
+#
+def label_dcmfiles(dcm_root, dcm_tag, tag_contains):
+    """
+    :param dcm_root:
+    :param dcm_tag:
+    :param tag_contains:
+    :return:
+    """
+    for subdir, _, files in os.walk(dcm_root):
+        if len(files) <= 0: continue
+        for file in files:
+            ds = pydicom.read_file(os.path.join(subdir, file))
+            if ds.get(dcm_tag) is None: continue
+            if tag_contains in str(ds.get(dcm_tag)):
+                new_filename = tag_contains + '_' + file
+                shutil.copy(os.path.join(subdir, file), os.path.join(subdir, new_filename))
+    return
+
+#----------------------------------------------------------------------------------------
+#
 # Test Purpose
 #
 #----------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    dcm_root = 'C:\\Users\\hui.liu02\\Desktop\\GABA\\gaba2'
+    #dcm_root = 'C:\\Users\\hui.liu02\\Desktop\\GABA\\gaba2'
     #series = dump_series2json(dcm_root)
     #df = pd.DataFrame(series)
     #df.to_excel('C:\\Users\\hui.liu02\\Desktop\\GABA\\gaba2.xlsx')
-    #"""
+    """
     cp_series(dcm_root,
               'C:\\Users\\hui.liu02\\Desktop\\GABA\\gaba2.xlsx',
               'C:\\Users\\hui.liu02\\Desktop\\GABA\\gaba')
-    #"""
+    """
+    """
+    diff_dcmfiles('C:\\Users\\hui.liu02\\Desktop\\GABA\\Cai^Fang\\20170703\\3_gaba\\44082289',
+                  'C:\\Users\\hui.liu02\\Desktop\\GABA\\Cai^Fang\\20170703\\3_gaba\\44082307')
+    """
+    label_dcmfiles('C:\\Users\\hui.liu02\\Desktop\\GABA', 'ImageComments', 'Difference')
